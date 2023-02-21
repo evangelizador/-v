5508e1b80a2fa8f53bb77526c97e7c59cfe029d0
Christian Flores <124392213+evangelizador@users.noreply.github.com>
2023-02-21 12:56:32 -0600

    Add files via upload

---
+++ EVA1_9_MENSAJE/src/eva1_9_mensaje/EVA1_9_MENSAJE.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_mensaje;
+
+/**
+ *
+ * @author Uptow
+ */
+public class EVA1_9_MENSAJE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        nPrintln("hola", 5);
+    }
+     public static void nPrintln(String message, int num){
+         for (int i = 0; i < 10; i++) {
+             System.out.println(message);
+             
+         }
+     }
+}
