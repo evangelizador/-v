7aadd4270f6f8bca52dd3379085407ee231b58ec
Christian Flores <124392213+evangelizador@users.noreply.github.com>
2023-02-23 16:18:34 -0600

    Add files via upload

---
+++ EVA1_13_ALCANCE_2/src/eva1_13_alcance_2/EVA1_13_ALCANCE_2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_13_alcance_2;
+
+/**
+ *
+ * @author Uptow
+ */
+public class EVA1_13_ALCANCE_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+            int x= 100;
+            System.out.println(x); 
+            {
+                int y = 100;
+                System.out.println(y);
+            }
+            System.out.println(y);
+        }
+        System.out.println(i);
+        System.out.println(x);
+    }
+    
+}
