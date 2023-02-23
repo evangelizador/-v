662dbf7e12d92334b66644a56cdcc720d925185f
Christian Flores <124392213+evangelizador@users.noreply.github.com>
2023-02-23 16:19:57 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author Uptow
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hllo world!!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    public static void Algo() {
+        
+    }
+    public static void Algo(int x) {
+        
+    }
+    
+}
