ffc6a02f2984e4d55f8e8ac68ef377c5ea239db0
Christian Flores <124392213+evangelizador@users.noreply.github.com>
2023-02-23 16:18:17 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_12_alcance;
+
+/**
+ *
+ * @author Uptow
+ */
+public class EVA1_12_ALCANCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(x);// no existe
+                              // poruqe esta declarada despues de la funcion
+                              // intruccion
+        System.out.println(args);//args declarado antes de la funcion
+                                   // instruccion 
+        int x = 100;
+        System.out.println(x);
+    }
+    
+}
